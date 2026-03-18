5ba04f9771f4fab8a8b6e3ac8e4873187cd119a7
kakouross <147107180+kakouross@users.noreply.github.com>
2026-03-18 17:54:48 +0200

    Update newton.c

--- newton.c
+++ newton.c
@@ -2,72 +2,54 @@
 #include <stdlib.h>
 #include <math.h>
 
-int isPrime(long long int p) {                                                                                      /*Συνάρτηση που ελέγχει αν ένας αριθμός είναι πρώτος*/                    
-    if (p <= 3) {                                                                                                   /*Αν ο αριθμός είναι μικρότερος του 3, είναι πρώτος*/
-        return 1;
-    }
+ double f(double a_0, double a_1, double a_2, double a_3, double a_4, double a_5, double x) {                                   /*Δήλωση της συνάρτησης f, που περιέχει το πολυόνυμο 5ου βαθμού*/
+    return (a_0 + a_1*x + a_2*x*x + a_3*x*x*x + a_4*x*x*x*x + a_5*x*x*x*x*x);
+ }
 
-    if (p % 2 == 0 || p % 3 == 0) {                                                                                 /*Αν ο αριθμός διαιρείται με το 2 ή το 3 δεν είναι πρώτος*/
-        return 0;
-    }
+ double f1(double a_0, double a_1, double a_2, double a_3, double a_4, double a_5, double x) {                                  /*Δήλωση της συνάρτησης f1, που περιέχει την παράγωγο της συνάρτησης f*/
+    return (0*a_0 + a_1*1 + a_2*2*x + a_3*3*x*x + a_4*4*x*x*x + a_5*5*x*x*x*x);
+ }
 
-    long long int i;
-    for (i = 5; i <= sqrt(p); i += 6) {                                                                             /*Εξετάζονται μόνο οι αριθμοί που δεν είναι πολλαπλάσιοι του 2 ή του 3*/
-        if (p % i == 0 || p % (i + 2) == 0) {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-
-int isPerfectSquare(double n) {                                                                                     /*Συνάρτηση που ελέγχει αν ένας αριθμός είναι τέλειο τετράγωνο πρώτου αριθμού*/
-    if( (trunc(n) == n) && (isPrime(n) == 1) ) {                                                                    /*Αν η ρίζα του αριθμού είναι ακέραιος και πρώτος τότε είναι τέλειο τετράγωνο*/
-        return 1;
-    } else return 0;
-}
-
-
-long long int mirror(long long int m) {                                                                             /*Συνάρτηση που βρίσκει το κάτοπτρο ενός αριθμού*/
-    long long int result = 0, tmp;
-    
-    while(m > 0) {
-        tmp = m % 10;                                                                                               /*tmp = τελευταίο ψηφίο του αριθμού*/
-        result = 10 * result + tmp;                                                                                 /*result = προστίθεται κάθε φορά το τελευταίο ψηφίο του αρχικού αριθμού*/
-        m /= 10;                                                                                                    /*Ο αρχικός αριθμός μειώνεται κατά ένα ψηφίο*/
-    }
-    
-    return result;
-}
-
- long long int j;
- long long int sum=0;
- long long int low;
- long long int high;
-
+ double x2;                                                                                                                  
+ int rootfound=0;
+ int i=1;
 
 int main(int argc, char **argv) {
-    if(argc != 3) {                                                                                                 /*Αν το πρόγραμμα δεν δέχεται δύο ορίσματα εμφανίζεται κατάλληλο μήνυμα*/
-        printf("2 numbers required\n");
+    
+    if(argc != 8) {
+        printf("7 numbers required\n");
         return 1;
     }
-
-    long long int low = atoll(argv[1]);
-    long long int high = atoll(argv[2]);
-
-    if(low>high || low<1 || low>1e15 || high <1 || high >1e15) {                                                    /*Έλεγχος εγκυρότητας των ορισμάτων που δώθηκαν*/
-        return 1;
+    
+    double a_0 = atof(argv[1]);
+    double a_1 = atof(argv[2]);
+    double a_2 = atof(argv[3]);
+    double a_3 = atof(argv[4]);
+    double a_4 = atof(argv[5]);
+    double a_5 = atof(argv[6]);
+    double x = atof(argv[7]);
+    
+                                                                                                                       
+    while(rootfound==0) {                                                                                                       /*Ο αλγόριθμος θα επαναλαμβάνεται μέχρι η μεταβλητή rootfound να λάβει την τιμή 1, μέχρι δηλαδή να βρεθεί ρίζα*/
+        x2 = x - f(a_0, a_1, a_2, a_3, a_4, a_5, x)/f1(a_0, a_1, a_2, a_3, a_4, a_5, x);                                        /*Στη μεταβλητή x2 εκχωρείται η τιμή της νέας ρίζας σύμφωνα με τη μέθοδο newton*/      
+        
+        if(fabs(x2-x) < 1.0e-6) {                                                                                               /*1η συνθήκη τερματισμού. Αν ισχύει τότε βρέθηκε ρίζα*/
+            rootfound=1;
+            break; }
+        else if(f1(a_0, a_1, a_2, a_3, a_4, a_5, x)==0) {                                                                       /*2η συνθήκη τερματισμού. Αν ο αλγόριθμος αποκλείνει τότε το πρόγραμμα εκτυπώνει nan και τερματίζει*/
+            printf("nan\n");
+            break; }
+        else if(i>1000) {                                                                                                       /*3η συνθήκη τερματισμού. Αν ο μετρητής επαναλήψεων i ξεπεράσει το 1000, τότε το πρόγραμμα τερματίζει*/
+            printf("incomplete\n");
+            break; }
+        
+        x=x2;                                                                                                                   /*Η μεταβλητή x λαμβάνει την τιμή της προηγούμενης ρίζας (x2) ώστε να συνεχιστεί ο αλγόριθμος*/
+        i++;                                                                                                                    /*Η μεταβλητή i αυξάνεται κάθε φορά κατά 1, ώστε να μετράει τον αριθμό επαναλήψεων*/
+    } 
+    
+    if(rootfound==1) {                                                                                                          /*Αν βρεθεί ρίζα τότε εκτυπώνεται η τιμή της*/
+        printf("%.2f\n", x2);
     }
-
-    long long int sqlow = sqrt(low);                                                                                /*Δημιουργία μεταβλητών που περιέχουν τη ρίζα των ορισμάτων για βελτιστοποίηση του αλγορίθμου*/
-    long long int sqhigh = sqrt(high);
-
-    for(j = ceil(sqlow); j <= sqhigh; j++) {                                                                        /*Έλεγχος για το αν ένας αριθμός είναι κατοπτρικό πρώτο τετράγωνο*/
-        if( (isPerfectSquare(j) == 1) && (isPerfectSquare(sqrt(mirror(j*j))) == 1) && (mirror(j*j) != j*j) ) { 
-            sum += j*j; 
-        }
-    }
-
-    printf("%lld\n", sum);
+    
     return 0;
 }
